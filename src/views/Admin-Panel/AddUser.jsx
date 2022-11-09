f94ab1d04c40693bf47d789ce6c86b8f24ec8d6a
Adrian H <adrian.henkel@student.hs-rm.de>
2022-11-09 15:00:15 +0100

    Added Admin Panel structure

---
+++ src/views/Admin-Panel/AddUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function AddUser() {
+    return (
+        <div>
+            Add User to "Users.json" here
+            Other functionalities like deleting, giving rights etc. next week?
+        </div>
+    );
+}
+
+export default AddUser;
---
+++ src/views/Admin-Panel/UserList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserList() {
+    return (
+        <div>
+            Add User to "Users.json" here
+            Other functionalities like deleting, giving rights etc. next week?
+        </div>
+    );
+}
+
+export default UserList;  
