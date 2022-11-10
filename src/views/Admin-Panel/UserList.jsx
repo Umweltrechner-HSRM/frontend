118b7c7734db819633ef4652a602d6b046e50183
Adrian Henkel <79994389+AdrianHenkel@users.noreply.github.com>
2022-11-10 11:19:57 +0100

    Update UserList.jsx
    Import data from AddUser

--- src/views/Admin-Panel/UserList.jsx
+++ src/views/Admin-Panel/UserList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { getUsers} from './AddUser';
 
 function UserList() {
+
+    const users = getUsers();
+
     return (
         <div>
-            Make List of Users and their data from "Users.json" here
-            Other functionalities like deleting, giving rights etc. next week?
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            List of Users <br/>
+            {users[0].name} <br/>
+            {users[0].password} <br/>
+            {users[0].rank} <br/>
+            {users[0].mail} <br/>
+            {users[0].number} <br/>
         </div>
     );
 }
