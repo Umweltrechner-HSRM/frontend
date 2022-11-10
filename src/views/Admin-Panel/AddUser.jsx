c250338c393843abc4baf257d188fc3e21440849
Adrian Henkel <79994389+AdrianHenkel@users.noreply.github.com>
2022-11-10 11:18:58 +0100

    Update AddUser.jsx
    Found a way to change and export datas dynamically

--- src/views/Admin-Panel/AddUser.jsx
+++ src/views/Admin-Panel/AddUser.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import {React} from 'react';
+
+export let userData = []; //container for data, list of Users
+
+//gives access to data
+export function getUsers(){
+    add_user("Peter", "w3cxm", "admin", "Peter@mail", "06122123456");
+    return userData;
+} 
+
+function add_user(new_name, new_password, new_rank, new_mail, new_number){
+    userData.push({
+        name: new_name,     //check name length, characters etc.
+        password: new_password,  //hashing later when security
+        rank: new_rank,  //user, admin, owner
+        mail: new_mail, //mail and number for alerts later
+        number: new_number //phone Number
+    })
+}
+
+
 function AddUser() {
+    var users = getUsers();
     return (
         <div>
-            Add User to "Users.json" here
-            Other functionalities like deleting, giving rights etc. next week?
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            Add Users <br/>
+            {users[0].name} <br/>
+            {users[0].password} <br/>
+            {users[0].rank} <br/>
+            {users[0].mail} <br/>
+            {users[0].number} <br/>
         </div>
     );
 }
+
 
 export default AddUser;
