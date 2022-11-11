03e6ab18300a39594139f020bd968d85628ee0c1
skyprime_17 <nmoeller@live.de>
2022-11-11 03:31:37 +0100

    - basic layout - basic pwa setup

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import {
   Divider,
   Flex,
 } from "@chakra-ui/react";
-import React, from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx";
 import Sidebar from "../components/Sidebar.jsx";
