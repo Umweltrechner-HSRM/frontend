efad02b5f0e9e90c40ac8cbf8bec592a374dff15
TaddelNunzn <Taddel1912@gmail.com>
2022-11-15 15:14:56 +0100

    Into the new Infrastructure

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,9 @@
 import { FiCalendar, FiHome, FiSettings } from "react-icons/fi";
+import {AiOutlineTable} from "react-icons/ai";
 import Dashboard from "./views/Dashboard.jsx";
 import Graph from "./views/Datasets.jsx";
 import Settings from "./views/Settings.jsx";
+import DataPreview from './views/DataPreview.jsx';
 import React from "react";
 
 const ClientRoutes = [
@@ -20,12 +22,19 @@ const ClientRoutes = [
     permission: null
   },
   {
+    title: "Datasets",
+    icon: AiOutlineTable,
+    path: "/datapreview",
+    element: <DataPreview />,
+    permission: "admin"  
+  },
+  {
     title: "Settings",
     icon: FiSettings,
     path: "/settings",
     element: <Settings />,
     permission: "admin"
-  }
+  },
 ];
 
 
