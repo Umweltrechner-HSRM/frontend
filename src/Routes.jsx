e1cacefd650af35e000588a5a24b60ece2465bb5
skyprime_17 <nmoeller@live.de>
2023-01-17 21:43:26 +0100

    - new design

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import { FiHome } from 'react-icons/fi';
 import { AiOutlineLineChart } from 'react-icons/ai';
+import { HiOutlineVariable } from 'react-icons/hi';
 import Dashboard from './views/Dashboard.jsx';
 import React from 'react';
 import ManageCharts from './views/ManageCharts.jsx';
 import FormulaEditor from "./views/FormelEditor.jsx";
 import VariablePage from "./views/Variable.jsx";
+import { TbMath } from "react-icons/tb";
 
 const ClientRoutes = [
   {
@@ -23,14 +25,14 @@ const ClientRoutes = [
   },
   {
     title: 'Formula Editor',
-    icon: AiOutlineLineChart,
+    icon: TbMath,
     path: '/formula',
     element: <FormulaEditor />,
     permission: 'admin'
   },
   {
     title: 'Variables',
-    icon: AiOutlineLineChart,
+    icon: HiOutlineVariable,
     path: '/variable',
     element: <VariablePage />,
     permission: 'admin'
